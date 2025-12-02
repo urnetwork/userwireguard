81b0dbd041a8b8aacf303c94ee4044914b610ec3
Product Builder <builder@bringyour.com>
2025-12-01 17:23:53 -0800

    2025.12.1-798384920

--- device/noise-protocol.go
+++ device/noise-protocol.go
@@ -15,7 +15,7 @@ import (
 	"golang.org/x/crypto/chacha20poly1305"
 	"golang.org/x/crypto/poly1305"
 
-	"github.com/urnetwork/userwireguard/tai64n"
+	"github.com/urnetwork/userwireguard/v2025/tai64n"
 )
 
 type handshakeState int
