4cd5ab70812e292d9aee7c07e7245f62386ac10a
Product Builder <builder@bringyour.com>
2025-12-09 12:22:25 -0800

    2025.12.9-805116310

--- device/device_test.go
+++ device/device_test.go
@@ -19,11 +19,11 @@ import (
 	"testing"
 	"time"
 
-	"github.com/urnetwork/userwireguard/conn"
-	"github.com/urnetwork/userwireguard/conn/bindtest"
-	"github.com/urnetwork/userwireguard/logger"
-	"github.com/urnetwork/userwireguard/tun"
-	"github.com/urnetwork/userwireguard/tun/tuntest"
+	"github.com/urnetwork/userwireguard/v2025/conn"
+	"github.com/urnetwork/userwireguard/v2025/conn/bindtest"
+	"github.com/urnetwork/userwireguard/v2025/logger"
+	"github.com/urnetwork/userwireguard/v2025/tun"
+	"github.com/urnetwork/userwireguard/v2025/tun/tuntest"
 	"golang.zx2c4.com/wireguard/wgctrl/wgtypes"
 )
 
