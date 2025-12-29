1f83b2cd6e078fb245a1c77e6d9954cd98974eee
Product Builder <builder@bringyour.com>
2025-12-28 23:00:34 -0800

    2025.12.28-821915110

--- device/receive.go
+++ device/receive.go
@@ -13,7 +13,7 @@ import (
 	"sync"
 	"time"
 
-	"github.com/urnetwork/userwireguard/conn"
+	"github.com/urnetwork/userwireguard/v2025/conn"
 	"golang.org/x/crypto/chacha20poly1305"
 	"golang.org/x/net/ipv4"
 	"golang.org/x/net/ipv6"
