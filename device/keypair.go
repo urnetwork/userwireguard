8e83cc015036fbdd15635a00342ed07f45f3edeb
Product Builder <builder@bringyour.com>
2026-02-08 21:11:00 -0800

    2026.2.8-858138120

--- device/keypair.go
+++ device/keypair.go
@@ -11,7 +11,7 @@ import (
 	"sync/atomic"
 	"time"
 
-	"github.com/urnetwork/userwireguard/replay"
+	"github.com/urnetwork/userwireguard/v2026/replay"
 )
 
 /* Due to limitations in Go and /x/crypto there is currently
